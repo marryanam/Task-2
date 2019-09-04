8d7e2888c30de8018f072edec36c2519e4a4cbf9
marryanam <marryana.m@gmail.com>
2019-09-04 22:25:13 +0300

    generate & delete items from localstorage

--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import Button from 'react-bootstrap/Button';
@@ -12,15 +12,28 @@ export default function CityList () {
     if(localStorage.getItem('list')){
        arr = localStorage.getItem('list').split(',');
     }
-    console.log(arr);
+    // console.log(arr);
 
     function addItem(name){
         arr.push(name);
         localStorage.setItem('list', arr);
     }
 
-    function fetchFrom(){
-        api('Kyiv');
+    function fetchFrom(e){
+        console.log(e.target.dataset.city);
+        api(e.target.dataset.city);
+    }
+    function DelateItem(e){
+        console.log(e.target.dataset.city);
+        // arr.remove(e.target.dataset.city);
+        arr.forEach(function(item, index, object) {
+          if (item === e.target.dataset.city) {
+            object.splice(index, 1);
+            console.log(arr);
+          }
+        });
+        localStorage.setItem('list', arr);
+        console.log(arr);
     }
     function api(name){
         fetch('http://api.openweathermap.org/data/2.5/weather?q='+name+'&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial')
@@ -40,35 +53,38 @@ export default function CityList () {
             console.log('Fetch Error :-S', err);
           });
     }
-
-    const [show, setShow] = useState(false);
-
-    const ViewItem = () => setShow(false);
-    const DelateItem = () => setShow(true);
+    function createListItems(){
+        console.log(arr);
+        arr.map(x => {
+            return(
+                <ListGroup.Item>
+                    <div> Kyiv </div>
+                    <ButtonToolbar>
+                        <Button variant="info" onClick={addItem}>Views</Button>
+                        <Button variant="danger" onClick={fetchFrom}>Delate</Button>
+                    </ButtonToolbar>
+                </ListGroup.Item>
+            )
+        });
+    }
+    useEffect(() => {
+      // createListItems();
+       console.log('ddddddrrrrr');
+    })
 
     return (
         <ListGroup>
-            <ListGroup.Item>
-                <div> Kyiv </div>
-                <ButtonToolbar>
-                    <Button variant="info" onClick={addItem}>Views</Button>
-                    <Button variant="danger" onClick={fetchFrom}>Delate</Button>
-                </ButtonToolbar>
-            </ListGroup.Item>
-            <ListGroup.Item>
-                <div> Lviv </div>
-                <ButtonToolbar>
-                    <Button variant="info" onClick={ViewItem} >Views</Button>
-                    <Button variant="danger" onClick={DelateItem}>Delate</Button>
-                </ButtonToolbar>
-            </ListGroup.Item>
-            <ListGroup.Item>
-                <div> Kyiv </div>
-                <ButtonToolbar>
-                    <Button variant="info" onClick={ViewItem}>Views</Button>
-                    <Button variant="danger" onClick={DelateItem}>Delate</Button>
-                </ButtonToolbar>
-            </ListGroup.Item>
+            {arr.map(x => {
+            return(
+                <ListGroup.Item>
+                    <div> {x} </div>
+                    <ButtonToolbar>
+                        <Button variant="info" onClick={fetchFrom} data-city={x}>Views</Button>
+                        <Button variant="danger" onClick={DelateItem} data-city={x}>Delate</Button>
+                    </ButtonToolbar>
+                </ListGroup.Item>
+            )
+        })}
         </ListGroup>
     );
 }
